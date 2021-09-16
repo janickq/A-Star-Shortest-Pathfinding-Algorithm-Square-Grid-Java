7d2f020070c21ab36bc1536f16b37d2a7b8242c6
janickq <81129332+janickq@users.noreply.github.com>
2021-09-16 14:36:10 +0800

    removed unzipped files
    figured out how to use jar files

--- src/PathMap.java
+++ src/PathMap.java
@@ -1,9 +1,6 @@
 
-
 import java.util.ArrayList;
 import java.util.HashMap;
-
-
 
 public class PathMap {
 
@@ -24,14 +21,13 @@ public class PathMap {
   public HashMap<String, Pose2d> namedPath = new HashMap<>();
   public ArrayList<Pose2d> Path = new ArrayList<>();
 
-
   public void getPose(Pose2d startpoint, Pose2d endpoint) {
     curPose = startpoint;
     endPose = endpoint;
-    Ai = Math.toIntExact(Math.round(((curPose.getTranslation().getX())/ 2.25)*n));
-    Aj = Math.toIntExact(Math.round(((curPose.getTranslation().getY()) / 4.5)*n));
-    Bi = Math.toIntExact(Math.round(((endPose.getTranslation().getX())/ 2.25)*n));
-    Bj = Math.toIntExact(Math.round(((endPose.getTranslation().getY())/ 4.5)*n));
+    Ai = Math.toIntExact(Math.round(((curPose.getTranslation().getX()) / 2.25) * n));
+    Aj = Math.toIntExact(Math.round(((curPose.getTranslation().getY()) / 4.5) * n));
+    Bi = Math.toIntExact(Math.round(((endPose.getTranslation().getX()) / 2.25) * n));
+    Bj = Math.toIntExact(Math.round(((endPose.getTranslation().getY()) / 4.5) * n));
   }
 
   public void getObstacles(Pose2d Obstacle) {
@@ -45,9 +41,9 @@ public class PathMap {
     gridSize[0] = 2.25 / n;
     matrix = new boolean[n][n];
   }
-  
+
   public boolean[][] generateMap() {
-    
+
     for (int x = 0; x < n; x++) {
       for (int y = 0; y < n; y++) {
         matrix[x][y] = true;
@@ -55,14 +51,14 @@ public class PathMap {
     }
 
     for (int i = 0; i < Obstacles.size(); i++) {
-      int x = Math.toIntExact(Math.round((Obstacles.get(i).getTranslation().getX()/ 2.25)*n));
-      int y = Math.toIntExact(Math.round((Obstacles.get(i).getTranslation().getY() / 4.5)*n));
+      int x = Math.toIntExact(Math.round((Obstacles.get(i).getTranslation().getX() / 2.25) * n));
+      int y = Math.toIntExact(Math.round((Obstacles.get(i).getTranslation().getY() / 4.5) * n));
 
-      //larger x boundary because rectangular grid boxes
-      for (int xboundary = 0; xboundary < n/10; xboundary++) {
+      // larger x boundary because rectangular grid boxes
+      for (int xboundary = 0; xboundary < n / 10; xboundary++) {
         // matrix[x + xboundary][y] = false;
         // matrix[x - xboundary][y] = false;
-        for (int yboundary = 0; yboundary < n/30; yboundary++) {
+        for (int yboundary = 0; yboundary < n / 30; yboundary++) {
           matrix[x + xboundary][y + yboundary] = false;
           matrix[x + xboundary][y - yboundary] = false;
           matrix[x - xboundary][y + yboundary] = false;
@@ -70,11 +66,10 @@ public class PathMap {
         }
       }
 
-
     }
     return matrix;
   }
-  
+
   public void calculate() {
 
     pathfinder.generateHValue(matrix, Ai, Aj, Bi, Bj, n, 1, 100, false, 1);
@@ -86,9 +81,9 @@ public class PathMap {
     }
 
   }
-  
+
   public void Reduce() {
-    
+
   }
 
 }
